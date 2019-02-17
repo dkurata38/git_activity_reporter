e765ff94fc8b85441a56742915c636b2fe7ef2d4
DaisukeKURATA <d.kruata.38@gmail.com>
2019-02-17 16:23:10 +0900

    GitHubからイベントを取得できるように修正. 一旦自分のリポジトリを取得できればよいので, Oauth2AccessTokenで認証を行う実装にしている.

--- src/main/scala/com/github/dkurata38/git_activity_reporter/infrastracture/client/git_event/github/GitHubEventClient.scala
+++ src/main/scala/com/github/dkurata38/git_activity_reporter/infrastracture/client/git_event/github/GitHubEventClient.scala
@@ -1,9 +1,11 @@
 package com.github.dkurata38.git_activity_reporter.infrastracture.client.git_event.github
 
+import java.time.{LocalDate, LocalDateTime, ZoneId}
+
 import com.github.dkurata38.git_activity_reporter.domain.git_account.GitAccount
 import com.github.dkurata38.git_activity_reporter.domain.git_event.GitEvent.Push
 import com.github.dkurata38.git_activity_reporter.domain.git_event.{GitEvent, GitEventClient, GitRepository}
-import org.eclipse.egit.github.core.client.PageIterator
+import org.eclipse.egit.github.core.client.{GitHubClient, PageIterator}
 import org.eclipse.egit.github.core.event.Event
 import org.eclipse.egit.github.core.service.EventService
 
@@ -12,7 +14,9 @@ import scala.collection.JavaConverters.collectionAsScalaIterableConverter
 
 class GitHubEventClient extends GitEventClient {
   override def getUserEvents(gitAccount: GitAccount): Seq[GitEvent] = {
-    val eventService = new EventService()
+    val eventClient = new GitHubClient()
+    eventClient.setOAuth2Token(gitAccount.accessToken)
+    val eventService = new EventService(eventClient)
     val eventPageIterator = eventService.pageUserEvents(gitAccount.gitUserName)
 
     parseEvent(eventPageIterator)
@@ -21,16 +25,15 @@ class GitHubEventClient extends GitEventClient {
   def parseEvent(eventPageIterator: PageIterator[Event]): Seq[GitEvent] = {
 
     @tailrec
-    def parseEventRecursive(eventPageIterator: PageIterator[Event], events: Seq[GitEvent]): Seq[GitEvent] = {
-      if (eventPageIterator.hasNext) {
-        return events
+    def parseEventRecursive(eventPageIterator: PageIterator[Event], initEvents: Seq[GitEvent]): Seq[GitEvent] = {
+      if (!eventPageIterator.hasNext) {
+        return initEvents
       }
 
-      events ++ eventPageIterator.next().asScala.map{e =>
-        e.getType match {
-          case "PushEvent" => new GitEvent(GitRepository(e.getRepo.getName, e.getRepo.getUrl), Push)
-        }
-      }
+      val events = initEvents ++ eventPageIterator.next().asScala
+          .filter(e => LocalDateTime.ofInstant(e.getCreatedAt.toInstant, ZoneId.systemDefault()).toLocalDate.isAfter(LocalDate.now().minusDays(7)))
+        .filter(e => e.getType == "PushEvent")
+        .map(e => new GitEvent(GitRepository(e.getRepo.getName, e.getRepo.getUrl), Push))
       parseEventRecursive(eventPageIterator, events)
     }
     parseEventRecursive(eventPageIterator, Nil)
