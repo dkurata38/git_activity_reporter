81e0b8839b21cf6f4872f6e5045473e961431d00
DaisukeKURATA <d.kruata.38@gmail.com>
2019-02-21 08:54:05 +0900

    ファイル移動

--- src/main/scala/com/github/dkurata38/git_activity_reporter/domain/git_account/IGitAccountRepository.scala
+++
@@ -1,7 +0,0 @@
-package com.github.dkurata38.git_activity_reporter.domain.git_account
-
-trait IGitAccountRepository {
-  def findAllByUserId(userId: Int): Seq[GitAccount]
-
-  def findOneByUserIdAndGitProviderId(userId: Int): Option[GitAccount]
-}
