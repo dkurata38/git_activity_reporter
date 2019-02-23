9bbdeb11b451f94071d0a2e24eae547a86ddbdb3
DaisukeKURATA <d.kruata.38@gmail.com>
2019-02-23 23:05:55 +0900

    不要なコードを破棄

--- src/main/scala/com/github/dkurata38/git_activity_reporter/domain/model/git/GitEvent.scala
+++ src/main/scala/com/github/dkurata38/git_activity_reporter/domain/model/git/GitEvent.scala
@@ -2,10 +2,4 @@ package com.github.dkurata38.git_activity_reporter.domain.model.git
 
 import com.github.dkurata38.git_activity_reporter.domain.`type`.GitEventType
 
-class GitEvent(val gitRepository: GitRepository, val eventType: GitEventType) {
-
-}
-
-object GitEvent {
-
-}
+class GitEvent(val gitRepository: GitRepository, val eventType: GitEventType)
