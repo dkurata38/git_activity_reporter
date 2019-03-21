9d55d43e60b6bec553c8708d899a2b4f8b52a8d2
DaisukeKURATA <d.kruata.38@gmail.com>
2019-03-21 18:34:25 +0900

    メッセージ送信コーディネータークラスを整理。

--- app/application/interactor/SendMessageUseCaseInteractor.scala
+++ app/application/interactor/SendMessageUseCaseInteractor.scala
@@ -1,5 +1,6 @@
-package application.coordinator
+package application.interactor
 
+import application.inputport.SendMessageUseCaseInputPort
 import application.repository.IUserTokenRepository
 import domain.model.social.SocialClientId.Twitter
 import domain.model.social.{SocialAccountRepository, SocialClientId, SocialMessage, SocialMessageRepository}
@@ -7,12 +8,12 @@ import domain.model.user_token.Token
 import javax.inject.{Inject, Singleton}
 
 @Singleton
-class SendMessageCoordinator @Inject() (
+class SendMessageUseCaseInteractor @Inject()(
                                          private val userTokenRepository: IUserTokenRepository,
                                          private val socialAccountRepository: SocialAccountRepository,
                                          private val socialMessageRepository: SocialMessageRepository
-                            ) {
-  def sendMessage(token: String, clientId: SocialClientId, message: String) = {
+                            ) extends SendMessageUseCaseInputPort{
+  override def sendMessage(token: String, clientId: SocialClientId, message: String) = {
     userTokenRepository.findByUserToken(Token(token)).flatMap{ userToken =>
       socialAccountRepository.findOneByUserIdAndSocialClientId(userToken.userId, Twitter)
     }.foreach{ socialAccount =>
