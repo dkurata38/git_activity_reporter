916b0dda30135095ae533e888b8fa1e66ffddc1b
DaisukeKURATA <d.kruata.38@gmail.com>
2019-05-03 16:22:49 +0900

    ログイン画面を修正（ルーティングの変更) 不要なGIT連携画面を削除（ログイン画面がその役割を兼ねるため）

--- app/adapter/web/controllers/SignUpController.scala
+++ app/adapter/web/controllers/SignUpController.scala
@@ -4,29 +4,36 @@ import java.util.UUID
 
 import application.cache.CacheRepository
 import application.inputport.{FindUserByTokenUseCaseInputPort, UserActivationUseCaseInputPort}
+import domain.git_account.GitClientId
+import domain.git_account.GitClientId.GitHub
 import javax.inject.{Inject, Singleton}
 import play.api.Configuration
 import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+import adapter.web.controllers.routes._
 
 @Singleton
 class SignUpController @Inject() (cc: ControllerComponents, config: Configuration, cacheRepository: CacheRepository, userActivationUseCaseInputPort: UserActivationUseCaseInputPort, findUserByTokenUseCaseInputPort: FindUserByTokenUseCaseInputPort) extends AbstractController(cc){
-  def linkGit = Action { implicit request: Request[AnyContent] =>
+  def linkGit(clientId: String) = Action { implicit request: Request[AnyContent] =>
     request.session.get("accessToken")
       .flatMap(accessToken => findUserByTokenUseCaseInputPort.getUserOf(accessToken))
-      .map(_ => Redirect(adapter.web.controllers.routes.SummaryController.index()))
+      .map(_ => Redirect(SummaryController.index()))
       .getOrElse{
         val accessToken = UUID.randomUUID().toString
-        cacheRepository.setCache(accessToken, "oauthPurpose", OauthPurpose.SingUp)
-        Ok(views.html.signup.git()).withSession(("accessToken", accessToken))
+        cacheRepository.setCache(accessToken, "oauthPurpose", OauthPurpose.SignIn)
+        val session = request.session + ("accessToken", accessToken)
+        GitClientId(clientId) match {
+          case GitHub => Redirect(GithubOAuthController.signIn()).withSession(session)
+          case _ => Redirect(HomeController.index())
+        }
       }
   }
 
   def complete = Action { implicit request: Request[AnyContent] =>
     request.session.get("accessToken").map{accessToken =>
         userActivationUseCaseInputPort.activate(accessToken) match {
-          case Right(token) => Redirect(adapter.web.controllers.routes.SummaryController.index()).withSession(("accessToken", token.value))
-          case Left(message) => Redirect(adapter.web.controllers.routes.HomeController.index()).flashing(("message" , message))
+          case Right(token) => Redirect(SummaryController.index()).withSession(("accessToken", token.value))
+          case Left(message) => Redirect(HomeController.index()).flashing(("message" , message))
         }
-    }.getOrElse(Redirect(adapter.web.controllers.routes.HomeController.index()))
+    }.getOrElse(Redirect(HomeController.index()))
   }
 }
